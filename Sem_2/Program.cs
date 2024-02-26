15d50ff3a14ae3deca714aa408753994a6fef2cb
Nikolay Litvinchuk <nikolajlitvinchuk1985@gmail.com>
2024-02-26 21:53:37 +0700

    add third task

--- Sem_2/Program.cs
+++ Sem_2/Program.cs
@@ -11,8 +11,23 @@
 //    Пример 487=>8^7=2097152
 // 1ая цифра: Сотни / 100; 2ая цифра: Десятки / 10 и % 10; 3я цифра: Еденицы % 10.
 
-int number = 947;
-int secondDigit = number / 10 % 10; // 947 / 10 % 10 (947 / 10 = 94 % 10 = 4)
-int thirdDigit = number % 10; //947 % 10 = 7
-int result = (int) Math.Pow ( secondDigit, thirdDigit);
-Console.WriteLine ($"{secondDigit} ^ {thirdDigit} = {result}");
+// int number = 947;
+// int secondDigit = number / 10 % 10; // 947 / 10 % 10 (947 / 10 = 94 % 10 = 4)
+// int thirdDigit = number % 10; //947 % 10 = 7
+// int result = (int) Math.Pow ( secondDigit, thirdDigit);
+// Console.WriteLine ($"{secondDigit} ^ {thirdDigit} = {result}");
+
+// Программа которая выводит третью с конца цифру заданного числа или сообщает, что третьей цифры нет.
+//      Пример: 456 >= 4
+//              7812 >= 8
+
+// int number = 925; // (number / 100) % 10
+// if (number >=100)
+// {
+//     int thirdDigitFromEnd = (number / 100) % 10;
+//     Console.WriteLine ($" Третья цифра с конца: {thirdDigitFromEnd}");
+// }
+// else 
+// {
+//     Console.WriteLine ("Третьей цифры нет");
+// }
