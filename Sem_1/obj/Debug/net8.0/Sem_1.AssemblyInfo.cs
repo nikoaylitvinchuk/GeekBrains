a1cf334ad439e154897dca938b84e5ab4628a5a5
Nikolay Litvinchuk <nikolajlitvinchuk1985@gmail.com>
2024-02-26 21:13:14 +0700

    add Sem_2

--- Sem_1/obj/Debug/net8.0/Sem_1.AssemblyInfo.cs
+++ Sem_1/obj/Debug/net8.0/Sem_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sem_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daf2cf327e7cac10485416c1b6a52c4a2a97eeef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09a0e8bcbc03cf6333f1d181621f1c050f2b7565")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sem_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sem_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Sem_2/Program.cs
+++ Sem_2/Program.cs
@@ -1,1 +1,18 @@
-﻿// Вычислить и сложить первую и третью цифру трёхзначного числа.
+﻿// // Вычислить и сложить первую и третью цифру трёхзначного числа.
+
+// int number = 453;
+// int firstDigit = number / 100; // 453/100=4 первая цифра
+// int thirdDigit = number % 10; // 453%10=3 третья цифра
+// int result = firstDigit * 10 + thirdDigit; //4*10+3=43
+// // $" Текст {result} Текст"
+// Console.WriteLine($"В числе {number} => {result}");
+
+// Напишите программу которая принимает  на вход и возводит вторую цифру этого числа в степень равную третьей цифре.
+//    Пример 487=>8^7=2097152
+// 1ая цифра: Сотни / 100; 2ая цифра: Десятки / 10 и % 10; 3я цифра: Еденицы % 10.
+
+int number = 947;
+int secondDigit = number / 10 % 10; // 947 / 10 % 10 (947 / 10 = 94 % 10 = 4)
+int thirdDigit = number % 10; //947 % 10 = 7
+int result = (int) Math.Pow ( secondDigit, thirdDigit);
+Console.WriteLine ($"{secondDigit} ^ {thirdDigit} = {result}");
